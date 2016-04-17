8cb61af3b317d4346908847f5d54d34b20537fc7
telsaiori <telsaiori@gmail.com>
2016-04-17 07:12:25 +0000

    finished add_to_products method

--- app.rb
+++ app.rb
@@ -12,10 +12,10 @@ Product.new(title: "LEGO Firehouse Headquarter", price: 199.99, stock: 0)
 
 puts Product.all.count # Should return 3
 
-# Product.new(title: "LEGO Iron Man vs. Ultron", price: 22.99, stock: 55)
+Product.new(title: "LEGO Iron Man vs. Ultron", price: 22.99, stock: 55)
 # Should return DuplicateProductError: 'LEGO Iron Man vs. Ultron' already exists.
 
-# nanoblock = Product.find_by_title("Nano Block Empire State Building")
+#nanoblock = Product.find_by_title("Nano Block Empire State Building")
 # firehouse = Product.find_by_title("LEGO Firehouse Headquarter")
 
 # puts nanoblock.title # Should return 'Nano Block Empire State Building'
--- lib/product.rb
+++ lib/product.rb
@@ -1,12 +1,31 @@
 class Product
-    
+    attr_reader :title
     @@products = []
     
     def initialize(options = {})
-        @@products << self
+        @title = options[:title]
+        add_to_products
     end
     
     def self.all
         @@products
+    end
+    
+    private
+    
+    def add_to_products
+        p "ADD PRODUCT#{@@products}"
+        @@products.each do |item|
+            p item.title
+            if item.title == @title
+                raise DuplicateProductError, "#{@title} already exists."
+            else
+                @@products << self
+                break
+            end
+        end
+        if @@products == []
+            @@products << self
+        end
     end
 end
